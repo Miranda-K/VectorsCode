8c35e317e4f85863f47f4d9b8fd650a536961387
Miranda-K <mirandayfkong@gmail.com>
2026-05-23 22:10:12 -0400

    main class

--- Line.java
+++ Line.java
@@ -1,1 +1,28 @@
-public class 
+public class Line {
+
+    private Vector start;
+    private Vector direction;
+
+    public Line(double startX, double startY, double startZ,
+                double dirX, double dirY, double dirZ) {
+        start = new Vector(startX, startY, startZ);
+        direction = new Vector(dirX, dirY, dirZ);
+    }
+
+    public Vector getStart() {
+        return start;
+    }
+
+    public Vector getDirection() {
+        return direction;
+    }
+
+    public boolean onLine(Vector v) {
+        Vector difference = v.subtract(start);
+        // if difference v is colinear with direction then the point lies on the line
+        return difference.isColinear(direction);
+
+    public String toString() {
+        return "l = " + start + " + t" + direction;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,1 +1,69 @@
 
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter line 1 START point (x y z):");
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
+        double z1 = input.nextDouble();
+
+        System.out.println("Enter line 1 DIRECTION vector (x y z):");
+        double dx1 = input.nextDouble();
+        double dy1 = input.nextDouble();
+        double dz1 = input.nextDouble();
+
+        System.out.println("Enter line 2 START point (x y z):");
+        double x2 = input.nextDouble();
+        double y2 = input.nextDouble();
+        double z2 = input.nextDouble();
+
+        System.out.println("Enter line 2 DIRECTION vector (x y z):");
+        double dx2 = input.nextDouble();
+        double dy2 = input.nextDouble();
+        double dz2 = input.nextDouble();
+
+        //creating objects
+        Line line1 = new Line(x1, y1, z1, dx1, dy1, dz1);
+        Line line2 = new Line(x2, y2, z2, dx2, dy2, dz2);
+
+        
+        //check if colinear
+        if (line1.getDirection().isColinear(line2.getDirection())) {
+
+            //check if coincident (is a point of one line on the other line)
+            if (line2.onLine(line1.getStart())) {
+                System.out.println("The lines are coincident.");
+            } else {
+                System.out.println("The lines are parallel.");
+            }
+
+        }
+        
+        //if not colinear
+        else {
+
+            //solve start1 + s(direction1) = start2 + t(direction2)
+            double s;
+            //use x coordinate to first solve for s and then check if consistent wiht all variables
+            s = (x2 - x1) / dx1;
+            //calculate point with s 
+            double intersectX = x1 + s * dx1;
+            double intersectY = y1 + s * dy1;
+            double intersectZ = z1 + s * dz1;
+
+            //check if this point is on line 2 (s from line 1 is consistent with line2)
+            Vector intersectionPoint = new Vector(intersectX, intersectY, intersectZ);
+            if (line2.onLine(intersectionPoint)) {
+                System.out.println("The lines intersect.");
+                System.out.println("Intersection point: "
+                    + intersectionPoint);
+            } else { //if the possible intersection point is not on line 2 (no intersect)
+                System.out.println("The lines are skew.");
+            }
+        }
+    }
+}
